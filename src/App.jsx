5e1f173e2fdf89202a3ee0a9a90eb9ce7f855706
Toha810 <tohahpro@gmail.com>
2023-09-11 22:16:16 +0600

    tailwind link add

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 className="font-6xl text-2xl text-center mt-24">Knowledge-Cafe</h1>
+
+    </>
+  )
+}
+
+export default App
